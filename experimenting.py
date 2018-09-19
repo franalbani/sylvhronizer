e301257697a51f0dde4884dc526eeaacab0f45a0
Francisco Albani <francisco.albani@gmail.com>
2018-09-19 14:55:25 -0300

    changes behaviour to discard all samples until first zero crossing

--- experimenting.py
+++ experimenting.py
@@ -40,12 +40,16 @@ def sylvhronizer(signal, samples_per_bit):
     phase = float(one_every)
     assert phase > 1
 
-    # First sample is always chosen:
-    ps = signal[0]
-    ks = [0]
-    output = [signal[0]]
+    # We will discard all samples before first transition:
+    # FIXME: this requires pre-slicing
+    k0 = min(k for k, d in enumerate(np.diff(signal)) if d != 0)
 
-    for k, s in enumerate(signal[1:], start=1):
+    # First sample is always chosen:
+    ks = [k0]
+    ps = signal[k0]
+    output = [ps]
+
+    for k, s in enumerate(signal[(k0+1):], start=k0+1):
         # New sample
         phase -= 1.0
 
@@ -79,26 +83,55 @@ def experiment(samples_per_bit, B=10000):
  
     ts, square = bits2square(bits, samples_per_bit)
  
-    ks, samples = sylvhronizer(square, samples_per_bit)
+    ks, _ = sylvhronizer(square, samples_per_bit)
  
-    recovered_bits = (0.5 * (samples + 1)).astype(np.int64)
+    recovered_bits = (0.5 * (square[ks] + 1)).astype(np.int64)
 
-    ber = np.mean(bits != recovered_bits)
+    # first_recovered_bit
+    frb = int(np.floor(ks[0] / samples_per_bit))
+
+    ber = np.mean(bits[frb:] != recovered_bits)
 
     return ber
 
 
+def aux(samples_per_bit):
+    B = 20
+    bits = np.random.randint(0, 2, size=B)
+ 
+    ts, square = bits2square(bits, samples_per_bit)
+ 
+    ks, _ = sylvhronizer(square, samples_per_bit)
+
+    recovered_bits = (0.5 * (square[ks] + 1)).astype(np.int64)
+
+    # first_recovered_bit
+    frb = int(np.floor(ks[0] / samples_per_bit))
+
+    ber = np.mean(bits[frb:] != recovered_bits)
+
+    plt.plot(ts, square, '.-', color='blue')
+    plt.plot(ts[ks], square[ks], 'o', color='red')
+    plt.title('%r\n%r' % (list(bits[frb:]), list(recovered_bits)))
+    plt.grid(True)
+    plt.show()
+    
+
 if __name__ == '__main__':
 
     spbs = np.arange(1.05, 5, 0.11)
-    bers = []
-    for spb in spbs:
-        r = experiment(spb)
-        bers.append(r)
+    b_sizes = np.array([10, 20, 50, 100, 200, 500, 1000, 2000, 5000])
+    BER = np.empty(shape=(len(spbs), len(b_sizes)))
+    for i, spb in enumerate(spbs):
+        for j, bs in enumerate(b_sizes):
+            BER[i, j] = experiment(spb, B=bs)
     
-    plt.plot(spbs, bers, 'o-')
-    plt.xlabel('Samples per bit')
-    plt.ylabel('BER')
-    plt.xticks(spbs)
-    plt.grid(True)
+    plt.imshow(BER)
+    plt.colorbar()
+#     plt.plot(spbs, bers, 'o-')
+    plt.xlabel('Block size')
+    plt.ylabel('Samples per bit')
+#    plt.grid(True)
+    plt.xticks(range(len(b_sizes)), b_sizes)
+    plt.yticks(range(len(spbs)), map(lambda s: '%0.2f' % s, spbs))
     plt.show()
